78d793f06a4ff7038ffd19ff78088a71f9de781f
Chris Dollin <ehog.hedge@gmail.com>
2015-12-18 16:25:15 +0000

    Added proper assert that the "right" items have come back from the geo query. Fixed its .conf (it had been referring to the wrong transform).

--- sapi-lib/src/test/java/com/epimorphics/simpleAPI/transformedQuery/TestRunTransforms.java
+++ sapi-lib/src/test/java/com/epimorphics/simpleAPI/transformedQuery/TestRunTransforms.java
@@ -7,8 +7,12 @@
 package com.epimorphics.simpleAPI.transformedQuery;
 
 
+import static org.junit.Assert.*;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +26,7 @@ import com.epimorphics.simpleAPI.query.ListQueryBuilder;
 import com.epimorphics.simpleAPI.query.QueryBuilder;
 import com.epimorphics.simpleAPI.query.impl.SparqlQueryBuilder;
 import com.epimorphics.simpleAPI.requests.Call;
+import com.epimorphics.simpleAPI.results.Result;
 import com.epimorphics.simpleAPI.results.ResultStream;
 import com.epimorphics.sparql.geo.GeoQuery;
 import com.epimorphics.sparql.terms.Var;
@@ -55,12 +60,24 @@ public class TestRunTransforms {
         ListQuery query = geoQB.geoQuery(x).build();
 		
         String queryString = query.toString();
-        System.err.println(">> query:\n" + queryString);
+        // System.err.println(">> query:\n" + queryString);
 		
 		ResultStream stream = source.query(query, new Call(endpoint, null));
-        
-		System.err.println(">> RESULTS:");
-		while (stream.hasNext()) System.err.println(">> result: " + stream.next());
+		
+		Set<String> obtained = new HashSet<String>();
+		
+		while (stream.hasNext()) {
+			Result next = stream.next();
+			obtained.add(next.asJson().get("@id").getAsString().value());
+		}
+		
+		Set<String> expected = new HashSet<String>();
+		expected.add("http://environment.data.gov.uk/public-register/waste-carriers-brokers/id/CBDU53093/site/0/location/0");
+		expected.add("http://environment.data.gov.uk/public-register/waste-carriers-brokers/id/CBDU53225/site/0/location/0");
+		expected.add("http://environment.data.gov.uk/public-register/waste-carriers-brokers/id/CBDU53548/site/0/location/0");
+		expected.add("http://environment.data.gov.uk/public-register/waste-carriers-brokers/id/CBDU52542/site/0/location/0");
+		
+		assertEquals(expected, obtained);
 		
 //        assertContains( queryString, "?id <http://jena.apache.org/spatial#withinCircle> (60.1 19.2 11.0) ." );
         
